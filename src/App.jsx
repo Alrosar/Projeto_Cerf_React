04b3048b79fa34b52f57d75f3291ca36c7be3be0
alrosar <alrosar@icloud.com>
2022-08-16 20:39:00 -0300

    Aula-33

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-
 import CardGenerator from './components/CardGenerator';
-import Cards from './components/Cards';
 import './styles/css/App.css';
 
 
@@ -12,7 +10,6 @@ function App() {
         <h1 className="title">Lista de Afazeres</h1>
         <h3 className="subtitle">Coloque aqui as suas tarefas do dia para ter um melhor controle</h3>
         <CardGenerator />
-        <Cards />
       </div>
     </div>
   );
--- src/components/CardGenerator/index.jsx
+++ src/components/CardGenerator/index.jsx
@@ -1,15 +1,34 @@
+
+import { useState, useRef, useEffect } from 'react';
 import '../../styles/css/CardGenerator.css';
+import Cards from '../Cards';
 
 const CardGenerator = () => { 
+    const [cards, setCards] = useState([]);
+    const inputRef = useRef();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Desativa o comportamento padráo do formulário, que é enviar o formulário para o servidor.
+        const text = inputRef.current.value;
+        setCards([...cards, text]);
+        inputRef.current.value = '';
+    }
+
+    useEffect(() => {
+        console.log(cards);
+    }, [cards])
+
     return (
         <div className="card-generator">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label for="task">Coloque a sua tarefa aqui</label>
-                <input type="text" name="task" />
+                <input type="text" name="task" ref={inputRef}/>
                 <div className="card-generator_options">
-                    <button>Criar</button>
+                    <button type="submit">Criar</button>
                 </div>
             </form>
+            <Cards info={cards}/>
         </div>
     )
 }
